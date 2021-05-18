ae5d4d58f834439e038780c7f2dfb5ec4903ddbc
sh4869 <nobuk4869@gmail.com>
2021-05-18 16:00:02 +0900

    fix error on request

--- main.ts
+++ main.ts
@@ -12,10 +12,18 @@ const experiment = async (name: string): Promise<PackageExperimentResult> => {
   const yarnresult = [];
   logger.info(`start experiments: ${name}`);
   for (let i = 0; i < COUNT_OF_EXPERIMENTS; i++) {
-    const r = await runAdd("fpms", name);
-    fpmsresult.push(r);
-    const r2 = await runAdd("yarn", name);
-    yarnresult.push(r2);
+    try {
+      const r = await runAdd("fpms", name);
+      fpmsresult.push(r);
+    } catch (e) {
+      logger.error(`error on fpms and ${name}`, e);
+    }
+    try {
+      const r2 = await runAdd("yarn", name);
+      yarnresult.push(r2);
+    } catch (e) {
+      logger.error(`error on yarn and ${name}`, e);
+    }
   }
   return { name, fpmsresult, yarnresult };
 };
