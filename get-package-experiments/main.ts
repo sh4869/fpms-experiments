d74392f7d2b024733401ca36956b7e527ccd7e69
sh4869 <nobuk4869@gmail.com>
2022-02-12 19:47:20 +0900

    update experiments script

--- get-package-experiments/main.ts
+++ get-package-experiments/main.ts
@@ -4,7 +4,7 @@ import { PackageExperimentResult } from "./src/type";
 import { configure, getLogger } from "log4js";
 import dayjs from "dayjs";
 
-const COUNT_OF_EXPERIMENTS = 10;
+const COUNT_OF_EXPERIMENTS = 3;
 const RESULT_PER_FILE = 5000;
 const logger = getLogger();
 
@@ -17,41 +17,52 @@ const experiment = async (name: string): Promise<PackageExperimentResult> => {
       const r = await runAdd("fpms", name);
       fpmsresult.push(r);
     } catch (e) {
-      logger.error(`error on fpms and ${name}`, e);
+      logger.error(`error on fpms and ${name}`, (e as any).message);
+      break;
     }
     try {
       const r2 = await runAdd("yarn", name);
       yarnresult.push(r2);
     } catch (e) {
-      logger.error(`error on yarn and ${name}`, e);
+      logger.error(`error on yarn and ${name}`, (e as any).message);
+      break;
     }
   }
   return { name, fpmsresult, yarnresult };
 };
 
+const CSV_HEADER = `package,version,depcount,${new Array(COUNT_OF_EXPERIMENTS)
+  .fill(0)
+  .map((_, i) => `yarn${i}`)
+  .join(",")},${new Array(COUNT_OF_EXPERIMENTS)
+  .fill(0)
+  .map((_, i) => `fpms${i}`)
+  .join(",")},yarn_ave,fpms_ave,diff`;
+
 const main = async () => {
   const names = JSON.parse(readFileSync(process.argv[2]).toString()) as string[];
   for (let i = 0; i < names.length / RESULT_PER_FILE; i++) {
-    logger.info(`start ${i}/${names.length / RESULT_PER_FILE}`);
     const start = i * RESULT_PER_FILE;
     const end = (i + 1) * RESULT_PER_FILE > names.length ? names.length : start + RESULT_PER_FILE;
+    logger.info(`start ${start} ~ ${end} (all package length: ${names.length})`);
     let result = [];
     for (let j = start; j < end; j++) {
-      result.push(await experiment(names[j]));
+      if (j % 100 === 0) logger.info(`${j}`);
+      const v = await experiment(names[j]);
+      result.push(v);
     }
-    writeFileSync(
-      `result/result.csv`,
-      "package,version,depcount,yarn1,yarn2,yarn3,yarn4,yarn5,yarn6,yarn7,yarn8,yarn9,yarn10,fpms1,fpms2,fpms3,fpms4,fpms5,fpms6,fpsm7,fpms8,fpms9,fpms10\n" +
-        result
-          .filter((z) => z.yarnresult.length > 0)
-          .map(
-            (v) =>
-              `${v.name},${v.yarnresult[0].version},${v.yarnresult[0].depscount},${v.yarnresult
-                .map((v) => v.mill)
-                .join(",")},${v.fpmsresult.map((v) => v.mill).join(",")}`
-          )
-          .join("\n")
-    );
+    const toCSV = result
+      .filter((z) => z.yarnresult.length > 0 && z.fpmsresult.length > 0)
+      .map((v) => {
+        const yarn_ave = v.yarnresult.map((v) => v.mill).reduce((l, r) => l + r, 0) / COUNT_OF_EXPERIMENTS;
+        const fpms_ave = v.fpmsresult.map((v) => v.mill).reduce((l, r) => l + r, 0) / COUNT_OF_EXPERIMENTS;
+        const diff = yarn_ave - fpms_ave;
+        return `${v.name},${v.yarnresult[0].version},${v.yarnresult[0].depscount},${v.yarnresult
+          .map((v) => v.mill)
+          .join(",")},${v.fpmsresult.map((v) => v.mill).join(",")},${yarn_ave},${fpms_ave},${diff}`;
+      })
+      .join("\n");
+    writeFileSync(`result/result${i}.csv`, `${CSV_HEADER}\n${toCSV}`);
   }
 };
 
--- get-package-experiments/src/add.ts
+++ get-package-experiments/src/add.ts
@@ -7,20 +7,29 @@ import { promisify } from "util";
 import { cleanYarn, countDeps, findVersion } from "./yarn_funcs";
 import { createTmpDir, prepareDir, removeTmpDir } from "./files_funcs";
 
-export const runAdd = async (command: "fpms" | "yarn", name: string): Promise<RequestResult> => {
+export const runAdd = async (
+  command: "fpms" | "yarn",
+  name: string,
+  debug: boolean = false
+): Promise<RequestResult> => {
   // 準備
   const dir = prepare(name);
   // 実行
-  const result = await execAddCommand(command, name, dir);
+  const result = await execAddCommand(command, name, dir, debug);
   // 後処理
   return after(result, name, dir);
 };
 
-const execAddCommand = async (command: string, name: string, dir: string): Promise<ProcessResult> => {
+const execAddCommand = async (
+  command: string,
+  name: string,
+  dir: string,
+  debug: boolean = false
+): Promise<ProcessResult> => {
   const start = process.hrtime.bigint();
-  const result = await promisify(exec)(`${command} add ${name} --verbose`, {
+  const result = await promisify(exec)(`${command} add ${name}`, {
     cwd: dir,
-    env: { ...process.env, DEBUG: "*" },
+    env: { ...process.env },
   });
   const end = process.hrtime.bigint();
   console.log(result.stdout);
